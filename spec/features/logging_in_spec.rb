c7426d40dfd9dc1f8398ff8ed0adee6eaf53b019
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-03 14:33:14 -0800

    update logging_in_spec.rb

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Logging in a user' do
   
   background do
-    visit root_path
+    visit login_path
   end
 
   scenario 'Logging in with no username and no password' do
@@ -12,7 +12,7 @@ feature 'Logging in a user' do
       fill_in 'Password', with: ''
     end
     click_button 'Login'
-    expect(page.current_path).to eq root_path
+    expect(page.current_path).to eq login_path
     expect(page).to have_content 'Please enter a username and password'
   end
 
@@ -22,13 +22,13 @@ feature 'Logging in a user' do
       fill_in 'Password', with: 'user111'
     end
     click_button 'Login'
-    expect(page.current_path).to eq root_path
+    expect(page.current_path).to eq login_path
     expect(page).to have_content 'Incorrect username or password'
   end
 
   scenario 'Logging in with correct username and password' do
     login_user
-    expect(page.current_path).to eq home_path
+    expect(page.current_path).to eq @user # @user = profile page
     expect(page).to have_content 'Welcome back'
   end
 
@@ -38,7 +38,7 @@ feature 'Logging out a user' do
 
   scenario 'User logs out' do
     login_user
-    expect(page.current_path).to eq home_path
+    expect(page.current_path).to eq @user
     click_button 'Logout'
     expect(page.current_path).to eq root_path
   end
@@ -47,11 +47,18 @@ end
 
 feature 'Authorization for non-logged in users' do
 
-  # non-logged in user CANNOT buy
-  scenario 'Trying to access home_path without loggin in' do
-    visit home_path
-    expect(page.current_path).to eq root_path
-    expect(page).to have_content 'Please log in'
+  background do
+    login_user
+    create_item
+    logout
+  end
+
+  # move to other feature spec?
+  scenario 'Trying to buy while logged out' do
+    visit @item
+    click_button 'Buy Now'
+    expect(page.current_path).to eq login_path
+    expect(page).to have_content 'You need to be logged in to buy this item'
   end
 
 end
@@ -63,17 +70,15 @@ feature 'Authorization for logged-in users' do
   end
 
   scenario 'Trying to access signup page while logged in' do
-    expect(page.current_path).to eq home_path
     visit signup_path
-    expect(page.current_path).to eq home_path
+    expect(page.current_path).to eq root_path
+    expect(page).to have_content 'You already have an account'
   end
 
   scenario 'Trying to access login page while logged in' do
-    expect(page.current_path).to eq home_path
     visit login_path
-    expect(page.current_path).to eq home_path
-    visit root_path
-    expect(page.current_path).to eq home_path
+    expect(page.current_path).to eq root_path
+    expect(page).to have_content 'You are already logged in's
   end
 
 end
@@ -88,10 +93,17 @@ def login_user
   click_button 'Login'
 end
 
+def create_item
+  @item = Item.create(
+    name: 'Item1', 
+    pic_url: 'Item1.jpg', 
+    description: 'First item',
+    price: 9.99)
+end
 
-
-
-
+def logout
+  click 'Logout'
+end
 
 
 
