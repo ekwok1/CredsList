4a6705b0dd8d3c20cea1b54b0d546271f4b84881
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-03 10:45:47 -0800

    write feature specs for creating items

--- spec/features/crud_on_items_spec.rb
+++ spec/features/crud_on_items_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 feature 'R in CRUD: reading items' do
 
-  scenario 'accessing items while not logged in' do
+  scenario 'Accessing items while not logged in' do
     visit home_path
     expect(page.current_path).to eq home_path
   end
 
-  scenario 'accessing items while logged in' do
+  scenario 'Accessing items while logged in' do
     login_user
     visit home_path
     expect(page.current_path).to eq home_path
@@ -16,6 +16,26 @@ feature 'R in CRUD: reading items' do
 end
 
 feature 'C in CRUD: creating items' do
+
+  scenario 'Trying to create items while not logged in' do
+    visit new_item_path
+    expect(page.current_path).to eq root_path
+    expect(page).to have_content 'Please log in'
+  end
+
+  scenario 'Creating item while logged in ' do
+    login_user
+    visit new_item_path
+    within '#create' do
+      fill_in 'Name', with: 'Item1'
+      fill_in 'Picture URL', with: 'Item1.jpg'
+      fill_in 'Descrption', with: 'First item'
+      fill_in 'Price', with: 9.99
+    end
+    click_button 'Create Item'
+    expect(page.current_path).to eq root_path
+    expect(page).to have_content 'Item1'
+  end
 
 end
 
