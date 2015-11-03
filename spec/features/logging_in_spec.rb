e5320cfb81f05d67a3beadf38e4fd9a517c16dd3
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-03 01:54:16 -0800

    write feature specs for logging in

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -7,22 +7,39 @@ feature 'Logging in a user' do
   end
 
   scenario 'Logging in with no username and no password' do
+    within '#login' do
+      fill_in 'Username', with: ''
+      fill_in 'Password', with: ''
+    end
+    click_button 'Login'
+    expect(page.current_path).to eq root_path
+    expect(page).to have_content 'Please enter a username and password'
   end
 
   scenario 'Logging in with incorrect username and/or incorrect password' do
+    within '#login' do
+      fill_in 'Username', with: 'user1@test.com'
+      fill_in 'Password', with: 'user111'
+    end
+    click_button 'Login'
+    expect(page.current_path).to eq root_path
+    expect(page).to have_content 'Incorrect username or password'
   end
 
   scenario 'Logging in with correct username and password' do
+    login_user
+    expect(page.current_path).to eq home_path
+    expect(page).to have_content 'Welcome back'
   end
 
 end
 
 def login_user
-  @user = User.create(email: "user1@test.com", password:"user1")
+  @user = User.create(email: 'user1@test.com', password: 'user1')
   visit root_path
-  within "#login" do
-    fill_in "Username", with: @user.email
-    fill_in "Password", with: @user.password
+  within '#login' do
+    fill_in 'Username', with: @user.email
+    fill_in 'Password', with: @user.password
   end
-  click_button "Login"
+  click_button 'Login'
 end
