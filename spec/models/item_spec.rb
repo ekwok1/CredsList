e4b5c44158e2a18a41f82b5637e8a4963db0101b
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-03 00:08:23 -0800

    write specs for item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+  
+  [:name, :description, :pic_url, :price].each do |prop|
+    it { should respond_to prop }
+  end
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :pic_url }
+  it { should validate_presence_of :price }
+
+  it { should belong_to :user }
+  it { should have_one :txn }
+
 end
