dd847041d91e3392c29be90f3d8503ab885ac365
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-03 00:08:50 -0800

    update specs for user model to include uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe User do
-  
+
   [:email, :password, :password_digest, :profilepic_url].each do |prop|
     it { should respond_to prop }
   end
 
   it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
 
   it { should have_secure_password }
 
