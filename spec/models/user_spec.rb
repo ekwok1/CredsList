51e5b376a7e13c062d29aaff1fa0e4bbaf84029d
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-03 00:05:38 -0800

    write specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  
+  [:email, :password, :password_digest, :profilepic_url].each do |prop|
+    it { should respond_to prop }
+  end
+
+  it { should validate_presence_of :email }
+
+  it { should have_secure_password }
+
+  it { should have_many :items }
+  it { should have_many :txns }
+
 end
