62e76c46a4625e8e0830ec6755519187ae471b89
RoseTHERESA <rose.minta.theresa@gmail.com>
2015-11-04 12:14:46 -0800

    add method for generating password reset token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ActiveRecord::Base
 
   has_many :items
   has_many :txns
+
+  def generate_password_reset_token!
+    update(password_reset_token: SecureRandom.urlsafe_base64(48))
+  end
   
 end
