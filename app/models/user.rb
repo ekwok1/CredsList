b83ead3a840181ba97bf57731bc20468e3b43fa5
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-04 10:28:43 -0800

    update signup form for new migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, :first_name, :last_name, presence: true, uniqueness: true
 
   has_secure_password
 
