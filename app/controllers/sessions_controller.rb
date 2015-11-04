f8257528e1946b429f04ccf936abdb61ddb871a2
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-04 13:20:26 -0800

    update prevent_loginsignup_when_logged_in in sessions controller to allow logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-  before_action :prevent_loginsignup_when_logged_in, except: [:home]
+  before_action :prevent_loginsignup_when_logged_in, except: [:home, :logout]
   # the index of all the items
   def home
     @items = Item.all.order(id: :desc)
