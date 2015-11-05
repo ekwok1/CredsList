267b5cd70116f3f8474c3398f6baed0795a340d5
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-05 12:44:25 -0800

    write create method for comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   def create
+    @item = Item.find params[:id]
+    @comment = @item.comments.build comment_params
+    if @comment.save
+      render json: @comment
+    else
+      render json: {errors: @comment.errors.full_messages}
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
