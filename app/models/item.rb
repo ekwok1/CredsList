c5cf6672f1c6499b5773a8494ecfd2f94f7eb74e
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-05 11:55:07 -0800

    add dependent: :destroy to allow deleting item and associated txn

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
   validates :name, :description, :pic_url, :price, presence: true
 
   belongs_to :user
-  has_one :txn
+  has_one :txn, dependent: :destroy
   
 end
 
