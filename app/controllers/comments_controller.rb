a2e8fd13823537dabfa1d5f68ce1eb65930e3d54
Eugene Kwok <eugenekwok11@gmail.com>
2015-11-06 09:27:50 -0800

    fix bug for making comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,8 @@ class CommentsController < ApplicationController
     @item = Item.find params[:id]
     @comment = @item.comments.build comment_params
     if @comment.save
-      @comment.update(user_id: @item.user.id)
-      @comment.update(first_name: @item.user.first_name)
+      @comment.update(user_id: current_user.id)
+      @comment.update(first_name: User.find(id: current_user.id).first_name)
       render json: @comment
     else
       render json: {errors: @comment.errors.full_messages}
